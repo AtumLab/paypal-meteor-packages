4b18f4d4282e4d388551655211ac90f763116209
AtumLab <atum.lab@gmail.com>
2013-11-26 16:40:08 +0700

    Update package.js

--- package.js
+++ package.js
@@ -2,10 +2,12 @@
 // description
 Package.describe({
 	summary: "Package paypal for meteor",
-	author: "particle4dev@gmail.com"
 });
-Npm.depends({ "paypal-rest-sdk": "0.5.0" });
+Npm.depends({ "paypal-rest-sdk": "0.6.4" });
 Package.on_use(function (api) {
 	// server
 	api.add_files(["server.js"], "server");
+	if (typeof api.export !== 'undefined') {
+        api.export('PAYPAL_API', "server");
+    }
 });
